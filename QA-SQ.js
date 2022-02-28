55dc29aacd6d18f9a55ea41c57d13175340e28b0
Akramali786a <80402480+Akramali786a@users.noreply.github.com>
2022-02-28 14:41:00 +0500

    Initialize circleci-project-setup

---
+++ QA-SQ.js
@@ -0,0 +1,5 @@
+module.exports = {
+    QUEEN_AMDI_OWNER: '923456745713', // Black Amda - Queen Amdi Owner
+    QUEEN_AMDI_TEAM: '923456745713', // Team - Queen Amdi TEAM
+    SEW_QUEEN_OWNER: '923456745713' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+};
